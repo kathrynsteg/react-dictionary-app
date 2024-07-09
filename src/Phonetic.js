468522de2b7bb006d57234318ea47bd1f5dc2774
kathrynsteg <kathrynsteg@gmail.com>
2024-07-08 23:20:25 -0500

    Added phonetics rendering

---
+++ src/Phonetic.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  return (
+    <div className="Phonetic">
+      <p>{props.phonetic}</p>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Result(props) {
   if (props.definition) {
     return (
       <div className="Result">
         <h2>{props.definition.word}</h2>
+        <Phonetic phonetic={props.definition.phonetic} />
         {props.definition.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
